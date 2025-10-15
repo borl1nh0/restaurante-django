1c17d5e053e3ed45d2d3403532ce1f120646e946
Juan Antonio Sancho Hidalgo <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-10-15 17:50:49 +0200

    URL01: index + handlers de error

--- restaurante/views.py
+++ restaurante/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q, Count, Sum, Avg
+from .models import Restaurante, Direccion, Plato, Etiqueta, Mesa, Cliente, PerfilCliente, Reserva, Pedido, LineaPedido
+
+def index(request):
+    """
+    enlaces a las 10 URLs (índice).
+    
+    """
+    return render(request, 'restaurante/index.html')
+
+# Errores personalizados (punto 5)
+def error_400(request, exception): return render(request, 'restaurante/400.html', status=400)
+def error_403(request, exception): return render(request, 'restaurante/403.html', status=403)
+def error_404(request, exception): return render(request, 'restaurante/404.html', status=404)
+def error_500(request):             return render(request, 'restaurante/500.html', status=500)
--- restauranteBueno/urls.py
+++ restauranteBueno/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+handler400 = 'restaurante.views.error_400'
+handler403 = 'restaurante.views.error_403'
+handler404 = 'restaurante.views.error_404'
+handler500 = 'restaurante.views.error_500'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('restaurante.urls')),
 ]
