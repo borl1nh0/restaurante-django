247309f34473166c7a31b77bd87b9a411dd29e47
Juan Antonio Sancho Hidalgo <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-10-16 11:49:04 +0200

    modificacion de buscar_platos, buscar_simple y urls.py

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path  
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -7,10 +7,12 @@ urlpatterns = [
     path('restaurante/<int:id>/', views.detalle_restaurante, name='detalle_restaurante'),
     path('platos/', views.lista_platos, name='lista_platos'),
     path('platos/categoria/<str:categoria>/', views.platos_por_categoria, name='platos_por_categoria'),
+
+   
     path('platos/buscar/<str:texto>/<int:precio_min>/', views.buscar_platos, name='buscar_platos'),
+
     path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
     path('pedidos/sin-lineas/', views.pedidos_sin_lineas, name='pedidos_sin_lineas'),
     path('clientes/frecuentes/', views.clientes_frecuentes, name='clientes_frecuentes'),
     re_path(r'^buscar/(?P<texto>\w{3,20})/$', views.buscar_simple, name='buscar_simple'),
- 
 ]
