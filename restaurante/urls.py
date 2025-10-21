c3363aab0162f93e83d18708fdfee6cc9bda7bd0
Juan Antonio Sancho Hidalgo <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-10-21 18:02:12 +0200

    mejorar mensajes de error 400 y limpiar código en plantillas y urls

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -7,10 +7,7 @@ urlpatterns = [
     path('restaurante/<int:id>/', views.detalle_restaurante, name='detalle_restaurante'),
     path('platos/', views.lista_platos, name='lista_platos'),
     path('platos/categoria/<str:categoria>/', views.platos_por_categoria, name='platos_por_categoria'),
-
-   
     path('platos/buscar/<str:texto>/<int:precio_min>/', views.buscar_platos, name='buscar_platos'),
-
     path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
     path('pedidos/sin-lineas/', views.pedidos_sin_lineas, name='pedidos_sin_lineas'),
     path('clientes/frecuentes/', views.clientes_frecuentes, name='clientes_frecuentes'),
